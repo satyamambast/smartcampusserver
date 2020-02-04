480f96153b678f60d352f125ebf6cc0b86a3c55a
satyamambast <satyamambast@github.com>
2020-02-04 18:11:30 +0530

    new port

--- postest.py
+++ postest.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://172.31.43.205:8888/'
+url = 'http://122.174.136.121:54300/'
 myobj = {'percentage': '40','ID':'001','time':'6766767'}
 
 x = requests.post(url, data = myobj)
