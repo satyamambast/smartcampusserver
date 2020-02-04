b2a7ea527f52f1ddcb9202a7b5af624251c776d4
satyamambast <satyamambast@github.com>
2020-02-04 22:32:00 +0530

    djasd

--- postest.py
+++ postest.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://ec2-3-6-40-220.ap-south-1.compute.amazonaws.com:8090/'
-myobj = {'percentage': '40','ID':'001','time':'6766767'}
+myobj = {'type':'update','percentage': '40','ID':'001','time':'6766767'}
 
 x = requests.post(url, data = myobj)
 
