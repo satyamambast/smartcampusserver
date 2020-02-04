7bd1237f4621507b09c6ab4db325e073fd083423
satyamambast <satyamambast@github.com>
2020-02-04 22:17:12 +0530

    added sql support

--- postest.py
+++ postest.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://122.174.136.121:54300/'
+url = 'http://ec2-3-6-40-220.ap-south-1.compute.amazonaws.com:8090/'
 myobj = {'percentage': '40','ID':'001','time':'6766767'}
 
 x = requests.post(url, data = myobj)
--- posthandler.py
+++ posthandler.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
-
+import mysql.connector
+class DB:
+    def __init__(self):
+        self.mydb = mysql.connector.connect(host="localhost",user="root",passwd="root", database="smartdustbin")
+        self.mycursor = self.mydb.cursor()
+    def inserttodustbin(self,id,perc):
+        update_query="""UPDATE data SET per==%s where id='%s'"""
+        update_tuple=(perc,id)
+        self.mycursor.execute(update_query,update_tuple)
+        self.mydb.commit()
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
+        self.DaBa=DB()
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
@@ -20,12 +23,15 @@ class S(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
-
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself)
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        post_data=post_data.decode('utf-8')
+        d = dict(x.split("=") for x in post_data.split("&"))
+        if d["type"]=="update":
+            self.DaBa.inserttodustbin(d["id"],d["percentage"])
+
+
 
 def run(server_class=HTTPServer, handler_class=S, port=8090):
     logging.basicConfig(level=logging.INFO)
